58f0601bf2ab3daca802d5c158d1d4717ec967b7
jirapat kanokmaneeporn <ka.jirapat@gmail.com>
2018-11-13 16:29:01 +0700

    error fix

--- Practice4/Practice4/Form1.vb
+++ Practice4/Practice4/Form1.vb
@@ -2,22 +2,22 @@
 
     Private Sub ButtonSum_Click(sender As Object, e As EventArgs) Handles ButtonSum.Click
         Dim result As Double = (CDbl(TextBoxNumber1.Text) + CDbl(TextBoxNumber2.Text))
-        TextBoxTotal.Text = Format(result),"#,###.00"
+        TextBoxTotal.Text = Format(result, "#,###.00")
     End Sub
 
     Private Sub ButtonMinus_Click(sender As Object, e As EventArgs) Handles ButtonMinus.Click
         Dim result As Double = (CDbl(TextBoxNumber1.Text) - CDbl(TextBoxNumber2.Text))
-        TextBoxTotal.Text = Format(result),"#,###.00"
+        TextBoxTotal.Text = Format(result, "#,###.00")
     End Sub
 
     Private Sub ButtonMultiply_Click(sender As Object, e As EventArgs) Handles ButtonMultiply.Click
         Dim result As Double = (CDbl(TextBoxNumber1.Text) * CDbl(TextBoxNumber2.Text))
-        TextBoxTotal.Text = Format(result),"#,###.00"
+        TextBoxTotal.Text = Format(result, "#,###.00")
     End Sub
 
     Private Sub ButtonDivision_Click(sender As Object, e As EventArgs) Handles ButtonDivision.Click
         Dim result As Double = (CDbl(TextBoxNumber1.Text) / CDbl(TextBoxNumber2.Text))
-        TextBoxTotal.Text = Format(result),"#,###.00"
+        TextBoxTotal.Text = Format(result, "#,###.00")
     End Sub
 
     Private Sub ButtonClear_Click(sender As Object, e As EventArgs) Handles ButtonClear.Click
