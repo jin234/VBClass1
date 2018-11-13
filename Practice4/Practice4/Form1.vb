8a0d570707d4f66f2f11314aad100b3fdd56d616
jirapat kanokmaneeporn <ka.jirapat@gmail.com>
2018-11-13 16:18:31 +0700

    Add Code.

--- Practice4/Practice4/Form1.vb
+++ Practice4/Practice4/Form1.vb
@@ -1,3 +1,28 @@
 ﻿Public Class Form1
 
+    Private Sub ButtonSum_Click(sender As Object, e As EventArgs) Handles ButtonSum.Click
+        Dim result As Double = (CDbl(TextBoxNumber1.Text) + CDbl(TextBoxNumber2.Text))
+        TextBoxTotal.Text = (result)
+    End Sub
+
+    Private Sub ButtonMinus_Click(sender As Object, e As EventArgs) Handles ButtonMinus.Click
+        Dim result As Double = (CDbl(TextBoxNumber1.Text) - CDbl(TextBoxNumber2.Text))
+        TextBoxTotal.Text = (result)
+    End Sub
+
+    Private Sub ButtonMultiply_Click(sender As Object, e As EventArgs) Handles ButtonMultiply.Click
+        Dim result As Double = (CDbl(TextBoxNumber1.Text) * CDbl(TextBoxNumber2.Text))
+        TextBoxTotal.Text = (result)
+    End Sub
+
+    Private Sub ButtonDivision_Click(sender As Object, e As EventArgs) Handles ButtonDivision.Click
+        Dim result As Double = (CDbl(TextBoxNumber1.Text) / CDbl(TextBoxNumber2.Text))
+        TextBoxTotal.Text = (result)
+    End Sub
+
+    Private Sub ButtonClear_Click(sender As Object, e As EventArgs) Handles ButtonClear.Click
+        TextBoxNumber1.Text = ""
+        TextBoxNumber2.Text = ""
+        TextBoxTotal.Text = ""
+    End Sub
 End Class
