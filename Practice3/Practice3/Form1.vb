e30bd42bd17c7fd24522d96c176db0f44ab35dfe
jirapat kanokmaneeporn <ka.jirapat@gmail.com>
2018-11-13 16:27:41 +0700

    change vat fomular

--- Practice3/Practice3/Form1.vb
+++ Practice3/Practice3/Form1.vb
@@ -1,16 +1,16 @@
 ﻿Public Class Form1
     Private Sub Button7p_Click(sender As Object, e As EventArgs) Handles Button7p.Click
         Dim vat As Double = 7
-        TextBoxTotal.Text = Format(CDbl(TextBoxPrice.Text) * (vat + 100) / 100),"#,###.00"
+        TextBoxTotal.Text = Format(CDbl(TextBoxPrice.Text) * (100 - vat) / 100, "#,###.00")
     End Sub
 
     Private Sub Button10p_Click(sender As Object, e As EventArgs) Handles Button10p.Click
         Dim vat As Double = 10
-        TextBoxTotal.Text = Format(CDbl(TextBoxPrice.Text) * (vat + 100) / 100),"#,###.00"
+        TextBoxTotal.Text = Format(CDbl(TextBoxPrice.Text) * (100 - vat) / 100, "#,###.00")
     End Sub
 
     Private Sub Button15p_Click(sender As Object, e As EventArgs) Handles Button15p.Click
         Dim vat As Double = 15
-        TextBoxTotal.Text = Format(CDbl(TextBoxPrice.Text) * (vat + 100) / 100),"#,###.00"
+        TextBoxTotal.Text = Format(CDbl(TextBoxPrice.Text) * (100 - vat) / 100, "#,###.00")
     End Sub
 End Class
