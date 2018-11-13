00e37c3c67a63819705928fe94718b68efc45bfb
jirapat kanokmaneeporn <ka.jirapat@gmail.com>
2018-11-13 16:00:33 +0700

    Chane Int to double. Fix Textboxnum2 clear.

--- Practice2/Practice2/Form1.vb
+++ Practice2/Practice2/Form1.vb
@@ -1,10 +1,10 @@
 ﻿Public Class Form1
     Private Sub ButtonSum_Click(sender As Object, e As EventArgs) Handles ButtonSum.Click
-        MessageBox.Show(CInt(TextBoxNumber1.Text) + CInt(TextBoxNumber2.Text))
+        MessageBox.Show("Sum Is : " & (CDbl(TextBoxNumber1.Text) + CDbl(TextBoxNumber2.Text)), "Sum of Num1 and Num2", MessageBoxButtons.OK, MessageBoxIcon.Information)
     End Sub
 
     Private Sub ButtonClear_Click(sender As Object, e As EventArgs) Handles ButtonClear.Click
         TextBoxNumber1.Text = " "
-        TextBoxNumber1.Text = " "
+        TextBoxNumber2.Text = " "
     End Sub
 End Class
